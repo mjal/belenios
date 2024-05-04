29856ada5ca24e8cd99816bc9d84c109a5f7e62a
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-05-04 12:24:50 +0200

    Snapshot full of comments

--- src/minilib/main.ml
+++ src/minilib/main.ml
@@ -1,13 +1,6 @@
 open Cmdliner
 open Belenios_core
 
-(*
-let g = Ed25519_pure.g;;
-let challenge = "1337099679088420191945735243092362180482631661342475760186294036356510222127";;
-let response = "584715790211560003328841421218527705726927443831262760529448472738263625037";;
-let credential = "6d976b5f243bfada5f744b2dfd9a0a19784c89221c981e686c1aeca0dcc04ea2";;
-*)
-
 let read_file filename =
   let channel = open_in filename in
   try
@@ -19,12 +12,10 @@ let read_file filename =
     close_in_noerr channel;
     raise e
 
-(* Read the file ballot.json from the filesystem *)
 let ballot_str = read_file "ballot.json";;
 
 module PSerializable_j = Serializable_j
 open Belenios_core
-
 let main _ballot =
   let module G = (val Belenios_v1.Group.of_string "Ed25519") in
   let ballot = Belenios_v1.Serializable_j.ballot_of_string
@@ -33,19 +24,89 @@ let main _ballot =
   let credential = G.to_string ballot.credential in
   match ballot.signature with
   | Some ballot_sig ->
+      (*
       let challenge =  (G.Zq.to_string ballot_sig.s_proof.challenge) in
       let response  =  (G.Zq.to_string ballot_sig.s_proof.response) in
-      let hash      =  ballot_sig.s_hash in
-      Printf.printf "\n";
-      Printf.printf "Hash      : %s\n" hash;
       Printf.printf "Challenge : %s\n" challenge;
       Printf.printf "Response  : %s\n" response;
+      *)
+      let challenge_hex =  (Z.format "%x" (Z.of_string (G.Zq.to_string ballot_sig.s_proof.challenge))) in
+      let response_hex  =  (Z.format "%x" (Z.of_string (G.Zq.to_string ballot_sig.s_proof.response))) in
+      Printf.printf "Challenge (hex) : %s\n" challenge_hex;
+      Printf.printf "Response  (hex) : %s\n" response_hex;
       Printf.printf "Credential: %s\n" credential;
 
+      match ballot.signature with
+      | Some { s_hash; s_proof = { challenge; response } } ->
+         (* TODO: parse credential to a group element *)
+         let credential = G.of_string credential in
+         let commitment = G.(
+          (g **~ response) *~ (credential **~ challenge)
+         ) in
+          let _ = s_hash in
+          let () = Printf.printf "Commitment : %s\n" (G.to_string commitment) in
+         ()
+         (*
+           let prefix = make_sig_prefix s_hash in
+           Zq.(challenge =% G.hash prefix [| commitment |])
+         *)
+      | None -> ();
+
+      (*
+      match ballot_sig.s_hash with
+      | 
+      (*let g = Ed25519_pure.g in*)
+      let commitment = Ed25519_pure.(
+        g **~ (ballot_sig.s_proof.response)
+      ) in
+      *)
+
+      (* *~ (ballot.credential **~ (G.Zq.of_string challenge_hex)) in *)
+
+      (*
+       s_hash = expected_hash && G.check credential
+       &&
+       let commitment = (g **~ response) *~ (credential **~ challenge) in
+       let prefix = make_sig_prefix s_hash in
+       Zq.(challenge =% G.hash prefix [| commitment |])
+      *)
+
+      (*
+      let hash      =  ballot_sig.s_hash in
       Printf.printf "Ballot: %s\n" ballot_str;
       Printf.printf "Ballot hash: %s\n" (Belenios_core.Common_types.Hash.(
         unwrap (hash_string ballot_str)
       ));
+      *)
+
+      (*
+      let ballot_without_signature : (G.t, G.Zq.t) Belenios_v1.Serializable_t.ballot =
+        {
+          election_uuid = ballot.election_uuid;
+          election_hash = ballot.election_hash;
+          credential = ballot.credential;
+          answers = ballot.answers;
+          signature = None;
+        }
+      in
+
+      let serialized_ballot = 
+        Common.(
+          (Belenios_v1.Serializable_j.string_of_ballot (swrite G.to_string) (swrite G.Zq.to_string)
+             ballot_without_signature)
+        )
+      in
+      let s_hash  = Belenios_core.Common.sha256_b64 serialized_ballot in
+      let s_hash2 = Belenios_core.Common.sha256_hex serialized_ballot in
+      Printf.printf "serialized_ballot: %s\n" serialized_ballot;
+      Printf.printf "s_hash: %s\n" s_hash;
+      Printf.printf "s_hash2: %s\n" s_hash2;
+      *)
+
+      (*
+        (string_of_ballot (swrite G.to_string) (swrite G.Zq.to_string)
+           ballot_without_signature)
+       *)
 
       (*
       let election_uuid = ballot.election_uuid in
@@ -63,18 +124,10 @@ let main _ballot =
         (*   (Common.sread G.of_string) (Common.sread G.Zq.of_string) ballot_str in *)
       (*Printf.printf "Ballot without sig  : %s\n" ballot_without_signature_str in*)
       (* Printf.printf "Ballot without sig hash : %s\n" "TODO"; *)
-      ()
 
   | None -> Printf.printf "Signature is not present.\n";
-
   (*let signature = G.to_string b.signature in*)
-  Printf.printf "Here is your credential: %s\n" credential;;
-
-  (*
-  let ballot = int_of_string ballot in
-  let ballot = Belenios_core.Ballot.of_int ballot in
-  let ballot = Belenios_core.Ballot.to_string ballot in
-  *)
+  ()
 
 (* Program setup *)
 let setup =
