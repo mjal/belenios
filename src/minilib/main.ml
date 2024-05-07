6978fa71a6f9d22358c40b26aebbff575e9d6f16
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-05-07 17:44:56 +0200

    refactor checkSig

--- src/minilib/main.ml
+++ src/minilib/main.ml
@@ -14,14 +14,45 @@ let read_file filename =
 
 let ballot_str = read_file "ballot.json";;
 
-module PSerializable_j = Serializable_j
-open Belenios_core
+let module G = (val Belenios_v1.Group.of_string "Ed25519") in
+
+let n_to_string n = (Z.format "%x" (Z.of_string (G.Zq.to_string n)))
+in
+
+let checkSig (ballot : (G.t, G.Zq.t) Belenios_v1.Serializable_t.ballot) = 
+
+  match ballot.signature with
+  | None -> ()
+  | Some { s_hash; s_proof = { challenge; response } } ->
+    let commitment = G.(
+      (g **~ response) *~ (ballot.credential **~ challenge)
+    ) in
+    let _ = s_hash in
+    let () = Printf.printf "\n" in
+    let () = Printf.printf "Challenge : %s\n" (n_to_string challenge) in
+    let () = Printf.printf "Commitment (ou A?) : %s\n" (G.to_string commitment) in
+    let make_sig_prefix hash = "sig|" ^ hash ^ "|" in
+    let prefix = make_sig_prefix s_hash in
+    let () = Printf.printf "hashedString : %s\n" (prefix ^ G.to_string commitment) in
+    let _aa = Ed25519_copy.hash prefix [| (Ed25519_copy.of_string (G.to_string commitment)) |] in
+    let recomputedChallenge = G.hash prefix [| commitment |] in
+    let () = Printf.printf "recomputedChallenge : %s\n" (n_to_string recomputedChallenge) in
+    ()
+in
+
 let main _ballot =
-  let module G = (val Belenios_v1.Group.of_string "Ed25519") in
   let ballot = Belenios_v1.Serializable_j.ballot_of_string
     (Common.sread G.of_string) (Common.sread G.Zq.of_string) ballot_str in
-    (*(string_of_ballot (swrite G.to_string) (swrite G.Zq.to_string)*)
 
+  let () = checkSig ballot in
+
+  let g = G.g in
+  let () = Printf.printf "g : %s\n" (G.to_string (g)) in
+  let () = Printf.printf "h : %s\n" (G.to_string (G.invert g)) in
+  ()
+in
+
+  (*
   match ballot.signature with
   | Some { s_hash; s_proof = { challenge; response } } ->
     let challenge_int =  (G.Zq.to_string challenge) in
@@ -75,6 +106,7 @@ let main _ballot =
     let () = Printf.printf "Test h2   : %s\n" h2 in
     ()
   | None -> ()
+  *)
 
 (* Program setup *)
 let setup =
@@ -85,7 +117,10 @@ let setup =
       value & opt (some string) None & info [ "c"; "count" ] ~docv ~doc)
   in
   Term.(const main $ ballot)
-let info =
-  Cmd.info "belenios-minilib" ~version:"0.1" ~doc:"" ~man: []
+in
 
-let () = Cmd.eval (Cmd.v info setup) |> exit
+let info = Cmd.info "belenios-minilib" ~version:"0.1" ~doc:"" ~man: []
+in
+
+let () = Cmd.eval (Cmd.v info setup) |> exit in
+()
